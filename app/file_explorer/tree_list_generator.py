a4db533abc408c4a3fb306673562f97f1daa78b5
kasidis-manasurangkul <84066800+kasidis-manasurangkul@users.noreply.github.com>
2023-10-12 02:41:01 +0700

    change width of tree list to 250 and make function to add a new tree list when open new folder

--- app/file_explorer/tree_list_generator.py
+++ app/file_explorer/tree_list_generator.py
@@ -7,19 +7,41 @@ contain file inside that folder appear next to the previous tree list.
 
 Created by Mo, 12 October, 2023.
 '''
-from qtpy.QtWidgets import QMainWindow, QTreeView, QFileSystemModel
+from qtpy.QtWidgets import QMainWindow, QTreeView, QFileSystemModel, QSplitter
 from qtpy.QtCore import Qt
 
 class TreeListGenerator(QMainWindow):
     def __init__(self):
-        super().__init__()
+        super().__init()
+        self.splitter = QSplitter(Qt.Horizontal)
 
-        self.tree_view = QTreeView(self)
+        self.addFileTreeList(self.splitter, '')
+    
+    def folderOpened(self, index):
+        # Event handler for when a folder is opened (expanded)
+        folderPath = self.model.filePath(index)
+        
+        # Create a splitter to divide the window into parts for each tree view
+
+        # Create the initial file tree list
+        self.addFileTreeList(self.splitter, folderPath)
+        print(f"Folder opened: {folderPath}")
+
+    def addFileTreeList(self, splitter, startingPath):
+        # Create a new file tree list
+        self.treeView = QTreeView(self)
+        self.treeView.setFixedWidth(250)
         self.model = QFileSystemModel()
-        self.model.setRootPath("")
-        self.tree_view.setModel(self.model)
+        self.model.setRootPath(startingPath)
+        self.treeView.setModel(self.model)
 
-        self.tree_view.setRootIndex(self.model.index("/"))  # Set the root path
+        self.treeView.setRootIndex(self.model.index(startingPath))  # Set the root path
 
-        self.tree_view.setHeaderHidden(True)  # Optional: Hide header columns
-        self.tree_view.setColumnWidth(0, 250)  # Optional: Set the column width
+        self.treeView.setHeaderHidden(True)  # Optional: Hide header columns
+        self.treeView.setColumnWidth(0, 250)  # Optional: Set the column width
+        self.treeView.setColumnWidth(0, self.treeView.columnWidth(0) / 2)
+
+        # Add the new file tree list to the splitter
+        splitter.addWidget(self.treeView)
+        # Connect the expanded signal to an event handler
+        self.treeView.expanded.connect(self.folderOpened)
