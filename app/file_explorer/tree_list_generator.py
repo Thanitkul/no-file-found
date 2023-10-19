31e4bcf74a5e3b34d7cdb32891a906d21adac863
kasidismana <kasidismana@gmail.com>
2023-10-19 10:40:49 +0700

    remove attributes in tree list and add attribute table next to current tree list

--- app/file_explorer/tree_list_generator.py
+++ app/file_explorer/tree_list_generator.py
@@ -7,16 +7,35 @@ contain file inside that folder appear next to the previous tree list.
 
 Created by Mo, 12 October, 2023.
 '''
-from qtpy.QtWidgets import QMainWindow, QTreeView, QFileSystemModel, QSplitter
+from qtpy.QtWidgets import QMainWindow, QTreeView, QFileSystemModel, QSplitter, QTableWidgetItem, QTableWidget, QWidget, QVBoxLayout
 from qtpy.QtCore import Qt
 
 class TreeListGenerator(QMainWindow):
     def __init__(self):
         super().__init__()
         self.splitter = QSplitter(Qt.Horizontal)
-
-        self.addFileTreeList(self.splitter, '')
+        self.initUI()
     
+    def initUI(self):
+            # Create a container widget for the file attributes
+        self.attributeWidget = QWidget()
+        self.attributeLayout = QVBoxLayout(self.attributeWidget)
+
+        # Create a table to display file attributes
+        self.attributeTable = QTableWidget()
+        self.attributeTable.setColumnCount(2)
+        self.attributeTable.setHorizontalHeaderLabels(['Attribute', 'Value'])
+        self.attributeTable.horizontalHeader().setStretchLastSection(True)
+
+        # Add the attributeTable to the attributeLayout
+        self.attributeLayout.addWidget(self.attributeTable)
+
+        # Create the initial file tree list
+        self.addFileTreeList(self.splitter, '', isInitial=True)
+        self.splitter.addWidget(self.attributeWidget)
+
+        self.setCentralWidget(self.splitter)
+
     def folderOpened(self, index):
         # Event handler for when a folder is opened (expanded)
         folderPath = self.model.filePath(index)
@@ -27,21 +46,63 @@ class TreeListGenerator(QMainWindow):
         self.addFileTreeList(self.splitter, folderPath)
         print(f"Folder opened: {folderPath}")
 
-    def addFileTreeList(self, splitter, startingPath):
+    def addFileTreeList(self, splitter, startingPath, isInitial=False):
         # Create a new file tree list
         self.treeView = QTreeView(self)
         self.treeView.setFixedWidth(250)
         self.model = QFileSystemModel()
         self.model.setRootPath(startingPath)
+
         self.treeView.setModel(self.model)
+        self.treeView.hideColumn(1)
+        self.treeView.hideColumn(2)
+        self.treeView.hideColumn(3)
 
         self.treeView.setRootIndex(self.model.index(startingPath))  # Set the root path
+
 
         self.treeView.setHeaderHidden(True)  # Optional: Hide header columns
         self.treeView.setColumnWidth(0, 250)  # Optional: Set the column width
         # self.treeView.setColumnWidth(0, self.treeView.columnWidth(0) / 2)
 
         # Add the new file tree list to the splitter
-        splitter.addWidget(self.treeView)
+        if isInitial:
+            splitter.addWidget(self.treeView)
+        else:
+            self.splitter.insertWidget(self.splitter.count() - 1, self.treeView)
         # Connect the expanded signal to an event handler
         self.treeView.expanded.connect(self.folderOpened)
+        self.treeView.clicked.connect(self.fileClicked)
+
+    def fileClicked(self, index):
+        # Event handler for when a file is clicked
+        if index.isValid() and not self.model.isDir(index):
+            # File clicked, display attributes
+            self.fileSelected(index)
+
+    def fileSelected(self, index):
+        # Event handler for displaying file attributes
+        filePath = self.model.filePath(index)
+
+        # Get and display file attributes
+        attributes = {
+            'File Name': filePath.split('/')[-1],
+            'Last Modified': self.model.lastModified(index).toString(),
+            'File Size': f"{self.model.size(index) / 1024:.2f} KB"
+        }
+        print(attributes)
+
+        self.updateAttributeTable(attributes)
+
+    def updateAttributeTable(self, attributes):
+        # Clear the existing attribute table
+        self.attributeTable.setRowCount(0)
+
+        # Populate the attribute table with the new attributes
+        for key, value in attributes.items():
+            print(key, value)
+            rowPosition = self.attributeTable.rowCount()
+            self.attributeTable.insertRow(rowPosition)
+            self.attributeTable.setItem(rowPosition, 0, QTableWidgetItem(key))
+            self.attributeTable.setItem(
+                rowPosition, 1, QTableWidgetItem(value))
