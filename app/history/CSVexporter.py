b025f79e0dd0c768fc58c9065c644dae689136e6
bankvis7 <irbank7@gmail.com>
2023-11-02 16:40:05 +0700

    fix export cancel

--- app/history/CSVexporter.py
+++ app/history/CSVexporter.py
@@ -5,6 +5,9 @@ def exportAsCSV(csvColumnName, data):
         fileName = filedialog.asksaveasfilename(defaultextension=".csv",
                                                 initialfile="export",
                                                 filetypes=[("CSV Files", "*.csv"), ("All files", "*.*")])
+        if fileName == "":
+            print("Export cancelled")
+            return
         with open(fileName, 'w', newline='') as file:
             writer = csv.writer(file) 
             writer.writerow(csvColumnName)
